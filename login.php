1d6dd1f99d7771e035c14de6e93b5fb8f1b97e7a
Rodrigo-Filipe-Morais <115590961+Rodrigo-Filipe-Morais@users.noreply.github.com>
2022-12-02 20:55:29 +0000

    login funcional espero eu :)

--- login.php
+++ login.php
@@ -1,50 +1,89 @@
 <?php include_once("connectbd.php") ?>
 <!--
-<?php include_once("insert.php")?>
+
 -->
 
 <!DOCTYPE html>
 <html lang="en">
 <meta charset="UTF-8">
 
-<title>Login</title>
-<style>
-
-</style>
 <head>
-
+<title>Login</title>
 </head>
 
 <body>
-<?php
 
+<form action="login.php" method="post">
+<h1>Login</h1>
+<?php if (isset($_GET['error'])) { ?>
+        <p class="error"> <?php echo $_GET['error'] ?></p>
+<?php } ?>
 
-if(isset($_POST['name'])){
-$name = mysqli_real_escape_string($link, $_REQUEST['name']);
-$pass = mysqli_real_escape_string($link, $_REQUEST['pass']);
-
-// Attempt insert query execution
-$sql = "Select nome,password from Utilizador where nome='$name' and password='$pass'";
-if(mysqli_query($link, $sql )){
-    echo "encontrou.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
-}
-?>
-
-    <h1>Login</h1>
-
-<form action="" method="post" id="loginform">
     <p>
         <label for="name">Username:</label>
-        <input type="text" name="name" id="name">
+        <input type="text" name="name">
     </p>
     <p>
-    <label for="pass">Password:</label>
-    <input type="password" id="pass" name="pass">
+        <label for="pass">Password:</label>
+        <input type="password" name="password">
     </p>
-</form>
-<button type="submit" form="loginform" value="Submit">Enviar</button>
+    <button type="submit" >Login</button>
 
+</form>
+
+
+<?php
+function validate($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+session_start();
+if (isset($_POST['name']) && isset($_POST['password'])) {
+
+    $name = validate($_POST['name']);
+    $password = validate($_POST['password']);
+
+
+    echo "----".$password;
+
+
+
+
+    if (empty($name)) {
+        header("Location: login.php?error=User Name is requierd");
+        //exit();
+    } elseif (empty($password)) {
+        header("Location login.php?error=Password is requierd");
+        //exit();
+    }
+
+    $sql = "Select * FROM Utilizador Where nome='$name' and password='$password'";
+
+    $result = mysqli_query($link, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        echo "ENTREI";
+        $row = mysqli_fetch_assoc($result);
+        if ($row['nome'] == $name && $row['password'] == $password) {
+            echo "Logged IN!";
+            $_SESSION['nome'] = $row['name'];
+            $_SESSION['id'] = $row['id'];
+            $_SESSION['password'] = $row['password'];
+            echo "sd,jkgfsdk.jfasdçkj";
+            header("Location; index.php");
+            exit();
+        } else {
+            header("Location: login.php?error=incorrer username or password");
+            exit();
+        }
+    } else {
+        header("Location: login.php");
+    }
+}
+
+
+?>
 </body>
