6e48fe965e750191948ff3e6ccd88d9911160c96
Rodrigo-Filipe-Morais <115590961+Rodrigo-Filipe-Morais@users.noreply.github.com>
2022-12-02 18:30:24 +0000

    pagina de login funcional

--- connectbd.php
+++ connectbd.php
@@ -1,19 +1,19 @@
 <?php
 //nome do servidor e do user name(neste caso sao os mesmos)
-$server_name = "2022_DSOS_G2";
+$server_name = "ctesp.dei.isep.ipp.pt";
 $user_name = "2022_DSOS_G2";
 
 //password da connecão
 $password = "Er5ohRSHiLqLFf#";
 
-$database_name = "ficha7";
+$database_name = "2022_DSOS_G2";
 
 // Creating the connection by specifying the connection details
-$connection = mysqli_connect($server_name, $user_name, $password, $database_name);
+$link = mysqli_connect($server_name, $user_name, $password, $database_name);
 
-// Checking the  connection
-if (!$connection) {
-    die("Failed " . mysqli_connect_error());
-}
-echo "Connection established successfully";
+
+
+
+
+//mysqli_close($connection);
 ?>
--- login.php
+++ login.php
@@ -16,23 +16,35 @@
 </head>
 
 <body>
+<?php
 
-    <?php
 
-    ?>
+if(isset($_POST['name'])){
+$name = mysqli_real_escape_string($link, $_REQUEST['name']);
+$pass = mysqli_real_escape_string($link, $_REQUEST['pass']);
+
+// Attempt insert query execution
+$sql = "Select nome,password from Utilizador where nome='$name' and password='$pass'";
+if(mysqli_query($link, $sql )){
+    echo "encontrou.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+}
+?>
 
     <h1>Login</h1>
 
-<form action="" method="post">
+<form action="" method="post" id="loginform">
     <p>
-        <label for="username">Nome</label>
-        <input type="text" name="nome" id="nome">
+        <label for="name">Username:</label>
+        <input type="text" name="name" id="name">
     </p>
     <p>
-    <label for="pwd">Password:</label>
-    <input type="password" id="pwd" name="pwd">
+    <label for="pass">Password:</label>
+    <input type="password" id="pass" name="pass">
     </p>
 </form>
-
+<button type="submit" form="loginform" value="Submit">Enviar</button>
 
 </body>
